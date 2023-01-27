196c983cfed0921c284ca82536f9dc1eaa4368c2
Meek <meek10x@gmail.com>
2023-01-27 08:00:29 -0600

    a reusable button that can be customized

---
+++ apps/native/components/ConnectButton.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const ConnectButton = () => {
+  return (
+    <View>
+      <Text>ConnectButton</Text>
+    </View>
+  )
+}
+
+export default ConnectButton
