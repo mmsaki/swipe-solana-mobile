b5025d300bfb5f100c354b67931f3b6d66cffa00
Meek <meek10x@gmail.com>
2023-01-26 21:46:40 -0600

    addes screens for app

---
+++ apps/native/screens/SelectImages.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const SelectImages = () => {
+  return (
+    <View>
+      <Text>SelectImages</Text>
+    </View>
+  )
+}
+
+export default SelectImages
