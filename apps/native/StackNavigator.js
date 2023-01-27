9e71664ff10d26c7b96117583775226c7659111c
Meek <meek10x@gmail.com>
2023-01-27 08:01:08 -0600

    included an edit profile page

--- apps/native/StackNavigator.js
+++ apps/native/StackNavigator.js
@@ -4,26 +4,30 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './screens/HomeScreen';
 import ChatScreen from './screens/ChatScreen';
 import LoginScreen from './screens/LoginScreen';
+import EditProfile from './screens/EditProfile';
 import useAuth from './hooks/useAuth';
 
 const Stack = createNativeStackNavigator();
 
 const StackNavigator = () => {
-  const user = false;
+  const user = true;
   return (
-    <Stack.Navigator>
+    <Stack.Navigator
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
       {user ? (
         <>
-        <Stack.Screen name="Swipe" component={HomeScreen} />
-        <Stack.Screen name="Chat" component={ChatScreen} />
+          <Stack.Screen name="Swipe" component={HomeScreen} />
+          <Stack.Screen name="Chat" component={ChatScreen} />
+          <Stack.Screen name="Profile" component={EditProfile} />
         </>
       ) : (
-      <Stack.Screen name="Login" component={LoginScreen} />
-   )}
-      
-      
+        <Stack.Screen name="Login" component={LoginScreen} />
+      )}
     </Stack.Navigator>
   );
-}
+};
 
 export default StackNavigator
