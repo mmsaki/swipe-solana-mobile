993165da56a2fb338a05779d35cc5493f5cd2ffe
Meek <meek10x@gmail.com>
2023-01-26 21:44:46 -0600

    created useAuth context to be used later for wallet authentification

---
+++ apps/native/hooks/useAuth.js
@@ -0,0 +1,16 @@
+import React, {createContext, useContext} from 'react'
+
+const AuthContext = createContext({})
+
+export const AuthProvider = ({ children }) => {
+  return (
+    <AuthContext.Provider
+      value={{ user: "meek" }}>
+      {children}
+    </AuthContext.Provider>
+  )
+}
+
+export default function useAuth() {
+  return useContext(AuthContext)
+}
