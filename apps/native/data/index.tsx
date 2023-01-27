e3310aae2e14668504622e807b3ebdaf707de2fd
Meek <meek10x@gmail.com>
2023-01-26 21:44:12 -0600

    data to be used as placholders that are comming from the solana program

---
+++ apps/native/data/index.tsx
@@ -0,0 +1,118 @@
+export const profiles = [
+  {
+    publicKey: "7QucptjcBrQNfhcf9iDroaSvM9Q5F5nfYJLQBUepdJsK",
+    account: {
+      owner: "GrDXZruQ2Ypq3VBGT3UnDZVdf4Z8zGWTknQh6qvSjFCs",
+      userName: "msaki",
+      last_post: "1",
+      matches: "3",
+      uri: "https://images-ext-2.discordapp.net/external/S4VnatqXeS0QBb7odGHBmlVy9qVB3T2NtPthKEWSG_M/%3Fauto%3Dformat%26w%3D1400%26fr%3D1/https/i.seadn.io/gae/IHrlkHi2xGy9aHGTM-l0qPhkZtu4N-keVPD5zW1VLFu-R_MnITWDLL2a7KDccMozudQL0ils4nHKFpcf9m7GWsZ3MpWMmCZ4JMYwRWQ",
+    },
+  },
+  {
+    publicKey: "7QucptjcBrQNfhcf9iDroaSvM9Q5F5nfYJLQBUepdJsK",
+    account: {
+      owner: "DQvN6DeJQNE3iYMk7YR8o89f4kNHCmki1XbALY6ygKLp",
+      userName: "zenits",
+      last_post: "3",
+      matches: "2",
+      uri: "https://images-ext-1.discordapp.net/external/iP8GLw0aWdFhq_OUFX0_I7PLI2JUuTe4800ohb4C6wQ/%3Fauto%3Dformat%26w%3D1400%26fr%3D1/https/i.seadn.io/gae/igjc806dMSCUg9Jw8xnFSPgBPxD2gCBlixVqMVq7PcKcyN8612r5sGC4eYIAJjxcQYXqGHeUzYX0M7P-6aKYPgNneJwEnwsk8-Ce",
+    },
+  },
+  {
+    publicKey: "5RcZCQxfwu7XHJSE3HMtLXBw8b67gHK9V3jYi2drvzrY",
+    account: {
+      owner: "FeMKLRTCw7VbjM2N1ZntEeAT2TvhgBxe4k2tZ9AeqrCv",
+      username: "swindler",
+      lastPost: 0,
+      matches: 0,
+      uri: "https://i.seadn.io/gae/ZVVSA4Jtfc3tXMo053Xfus4K0PGdvvN1ZrUV863YPpv8xAjkpOTgDulwuEhuqr3mLuxxvc63R46cQi6ZMj0rhwDrp-kEt9Xz-JHcxw?auto=format&w=1400&fr=1",
+    },
+  },
+  {
+    publicKey: "7QucptjcBrQNfhcf9iDroaSvM9Q5F5nfYJLQBUepdJsK",
+    account: {
+      owner: "2Rekz32EkpAqk3BE1kjJrC8GuD8D2DWRxoHuU6EprNj2",
+      username: "test123",
+      lastPost: 1,
+      matches: 0,
+      uri: "https://images-ext-2.discordapp.net/external/fcwL-78hWWAXxvrbu7PvuNwCAa96DME3RN4S8xYbBKA/%3Fauto%3Dformat%26w%3D1400%26fr%3D1/https/i.seadn.io/gae/_89fP0a5Y_cyT4nkxHpak4BP7zKet26w1NGOXGuZ3ZLVuG7HJ5llq73f_N_86-TotEKU_g84YEzrQ-FmfMn7juLmdiokBCu8GeqdcQU",
+    },
+  },
+];
+
+export const posts = [
+  {
+    publicKey: "4B8TjiYnWsZ6V9kmmic67q3xWhUeQsegp6zFoDdqQavd",
+    account: {
+      owner: "2Rekz32EkpAqk3BE1kjJrC8GuD8D2DWRxoHuU6EprNj2",
+      id: 0,
+      title: "Retro Goons",
+      image:
+        "https://pbs.twimg.com/media/FnGoNpsacAAHuHs?format=jpg&name=large",
+    },
+  },
+  {
+    publicKey: "4ggWA3QQz2RmVorgLZ336ADnrCdHW1fS675F2WSaw9Lg",
+    account: {
+      owner: "DQvN6DeJQNE3iYMk7YR8o89f4kNHCmki1XbALY6ygKLp",
+      id: 0,
+      title:
+        "#345 | Group entering a white Felt Zine Egg scene with background seed 554 and a Shield (Swiss-German) card suit",
+      image:
+        "https://openseauserdata.com/files/df7b31c5a0908ac4efbe8620991b9567.png",
+    },
+  },
+  {
+    publicKey: "HRFAprQC99UceqzFQCBcArP98P1BHNAMEmzTin5795mK",
+    account: {
+      owner: "GrDXZruQ2Ypq3VBGT3UnDZVdf4Z8zGWTknQh6qvSjFCs",
+      id: 3,
+      title: "another day another dollar",
+      image: "https://pbs.twimg.com/media/FmsgMauacAAKkeJ.jpg",
+    },
+  },
+  {
+    publicKey: "JAEzwnmpCKTVVGZXAHeupqfPH1soQ1SSZdAX5PzaiMNL",
+    account: {
+      owner: "GrDXZruQ2Ypq3VBGT3UnDZVdf4Z8zGWTknQh6qvSjFCs",
+      id: 1,
+      title: "another post???",
+      image:
+        "https://ipfs.io/ipfs/QmbcVWJpL1Q3HQeb2DvMQqS4xH9qqJT1gBzWWroGwhvC8n",
+    },
+  },
+  {
+    publicKey: "Ajq79hHqmorPknom1VoDt1Yeyuhf1FGMAj6AMWcYLqV4",
+    account: {
+      owner: "GrDXZruQ2Ypq3VBGT3UnDZVdf4Z8zGWTknQh6qvSjFCs",
+      id: 2,
+      title: "hello",
+      image:
+        "https://f8n-production-collection-assets.imgix.net/0x32887799bA0d479fa9495c4f1dd28178b37068e5/1947/nft.png?q=80&auto=format%2Ccompress&cs=srgb&w=3000&h=3000&fit=max",
+    },
+  },
+  {
+    publicKey: "4T4Az3kADKi3JB84CsiXRCuVvab9vahq1YQaRP3iTGzN",
+    account: {
+      owner: "GrDXZruQ2Ypq3VBGT3UnDZVdf4Z8zGWTknQh6qvSjFCs",
+      id: 0,
+      title: "Felt Zine Logo #13844",
+      image:
+        "https://ipfs.io/ipfs/QmTrAUPPiy7sojNgMFz7P3Mc63nAX9DBRYzTY9cMW1Gtxc",
+    },
+  },
+];
+
+export const matches = [
+  {
+    publicKey: "GfNXD3SV2gMkiBsPH2UutTC4uX468TBT8S6pWwGEZdJd",
+    account: {
+      owner: "GrDXZruQ2Ypq3VBGT3UnDZVdf4Z8zGWTknQh6qvSjFCs",
+      user: "DQvN6DeJQNE3iYMk7YR8o89f4kNHCmki1XbALY6ygKLp",
+      image:
+        "#345 | Group entering a white Felt Zine Egg scene with background seed 554 and a Shield (Swiss-German) card suit",
+      title: "",
+    },
+  },
+];
