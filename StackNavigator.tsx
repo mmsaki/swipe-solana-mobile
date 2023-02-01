84ccd317bd20493d66cc2b5ce2f5a556f2c49053
Meek <meek10x@gmail.com>
2023-01-31 18:40:46 -0600

    feat: adding screen strack and groups

--- StackNavigator.tsx
+++ StackNavigator.tsx
@@ -1,11 +1,9 @@
 import "react-native-get-random-values";
 import "react-native-url-polyfill/auto";
-import { StatusBar } from "expo-status-bar";
-import React, { useEffect, useRef, useState, useMemo, useReducer } from "react";
+import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import Login from "./screens/Login";
-import Home from "./screens/Home";
 import HomeScreen from "./screens/HomeScreen";
 import Profile from "./screens/Profile";
 import Chat from "./screens/Chat";
@@ -13,6 +11,7 @@ import useGlobalAuth from "./state/useGlobalState";
 import ModalScreen from "./screens/ModalScreen";
 import LeaderBoardScreen from "./screens/LeaderBoardScreen";
 import CreateAccount from "./screens/CreateAccount";
+import MatchScreen from "./screens/MatchScreen";
 
 const Stack = createNativeStackNavigator();
 
@@ -20,9 +19,6 @@ export default function App() {
   const { phantomWalletPublicKey } = useGlobalAuth();
   return (
     <Stack.Navigator>
-      {/* <SafeAreaProvider>
-        <SafeAreaView style={styles.container}>
-          <View style={styles.header}> */}
       {phantomWalletPublicKey ? (
         <>
           <Stack.Group>
@@ -32,51 +28,22 @@ export default function App() {
           <Stack.Group
             screenOptions={{ presentation: "modal", headerShown: false }}
           >
-            <Stack.Screen name="Create Account" component={CreateAccount} />
             <Stack.Screen name="Profile" component={Profile} />
             <Stack.Screen name="Modal" component={ModalScreen} />
             <Stack.Screen name="LeaderBoard" component={LeaderBoardScreen} />
           </Stack.Group>
-
-          {/* <View style={[styles.row, styles.wallet]}>
-                  <View style={styles.greenDot} />
-                  <Text
-                    style={styles.text}
-                    numberOfLines={1}
-                    ellipsizeMode="middle"
-                  >
-                    {`Connected to: ${phantomWalletPublicKey.toString()}`}
-                  </Text>
-                </View>
-                <View style={styles.row}>
-                  <Button title="Add Review" onPress={openAddReviewSheet} />
-                  <Button title="Disconnect" onPress={disconnect} />
-                </View> */}
+          <Stack.Group
+            screenOptions={{ presentation: "formSheet", headerShown: false }}
+          >
+            <Stack.Screen name="Create Account" component={CreateAccount} />
+          </Stack.Group>
+          <Stack.Group screenOptions={{ presentation: "transparentModal" }}>
+            <Stack.Screen name="MatchScreen" component={MatchScreen} />
+          </Stack.Group>
         </>
       ) : (
-        <Stack.Screen name="Login" component={Login} />
-        // <View style={{ marginTop: 15 }}>
-        //   <Button title="Connect Phantom" onPress={connect} />
-        // </View>
-      )}
-      {/* </View> */}
-      {/* {submitting && (
-            <ActivityIndicator
-              color={COLORS.WHITE}
-              size="large"
-              style={styles.spinner}
-            />
-          )} */}
-      {/* <MovieList connection={connection} />
-          <AddReviewSheet
-            actionSheetRef={actionSheetRef}
-            phantomWalletPublicKey={phantomWalletPublicKey}
-            signAndSendTransaction={signAndSendTransaction}
-          />
-          <Toast config={toastConfig} />
-          <StatusBar style="auto" /> */}
-      {/* </SafeAreaView> */}
-      {/* </SafeAreaProvider> */}
+          <Stack.Screen name="Login" component={Login} />
+        )}
     </Stack.Navigator>
   );
 }
