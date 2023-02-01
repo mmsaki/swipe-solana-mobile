495e5f7525e2b80ee00764dbbe68f291236b2443
Meek <meek10x@gmail.com>
2023-02-01 17:39:47 -0600

    fix: walletcontext provider in global

--- App.tsx
+++ App.tsx
@@ -1,14 +1,17 @@
-import { StatusBar } from "expo-status-bar";
 import { NavigationContainer } from "@react-navigation/native";
 import StackNavigator from "./StackNavigator";
-import { GlobalProvider } from "./state/useGlobalState";
+import { WalletContextProvider } from "./hooks/WalletContextProvider";
+import { clusterApiUrl } from "@solana/web3.js";
+
+
+const devnet = clusterApiUrl("devnet");
 
 export default function Native() {
   return (
-    <NavigationContainer>
-      <GlobalProvider>
-        <StackNavigator />
-      </GlobalProvider>
-    </NavigationContainer>
+      <WalletContextProvider>
+        <NavigationContainer>
+          <StackNavigator />
+        </NavigationContainer>
+      </WalletContextProvider>
   );
 }
